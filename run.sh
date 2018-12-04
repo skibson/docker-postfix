d69357d126e2eaa5137345bf801682e2d85a415e
Michał Skibicki <mskibicki@9bits.pl>
2018-12-04 16:36:08 +0100

    sub

--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
- echo "******************************"
+echo "******************************"
 echo "**** POSTFIX STARTING UP *****"
 echo "******************************"
  # Make and reown postfix folders
 mkdir -p /var/spool/postfix/ && mkdir -p /var/spool/postfix/pid
 chown root: /var/spool/postfix/
 chown root: /var/spool/postfix/pid
+sed -i -r -e 's/^#submission/submission/' /etc/postfix/master.cf
 echo "- Staring rsyslog and postfix"
 exec supervisord -c /etc/supervisord.conf
